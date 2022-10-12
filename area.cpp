914e755baeb306512d8f1eadc8b447c2f84616e7
VISHAV GARG <92566550+vishavgarg16@users.noreply.github.com>
2022-10-12 20:05:49 +0530

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,b,a;
+    cout<<"enter the length:";
+    cin>>l;
+    
+    cout<<"enter the breadth:";
+    cin>>b;
+    
+    a=l*b;
+    
+    cout<<"area="<<a<<endl;
+    cout<<a<<" is area\n";
+}
